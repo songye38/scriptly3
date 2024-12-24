d98a96df4577adb361868329507cdf96cfd60388
songye38 <ddev0308@gmail.com>
2024-12-24 10:22:12 +0900

    UPDATE MyInput antd -> mui

--- src/Components/BasicComponents/MyInput.jsx
+++ src/Components/BasicComponents/MyInput.jsx
@@ -1,27 +1,29 @@
-// CustomInput.js
 import React from 'react';
-import { Input } from 'antd';
-
-// FIXME : 버튼을 인풋창 안으로 넣기
+import { TextField } from '@mui/material';
 
 const MyInput = ({ value, onChange, placeholder, disabled }) => {
   return (
-    <Input.TextArea
+    <TextField
       value={value}
       onChange={onChange}
       placeholder={placeholder}
-      rows={1}
-      disabled={disabled} // 추가
-      style={{
-        borderRadius: '8px',
-        border: '1px solid #BFBFBF',
-        padding: '8px 12px',
-        fontSize: '14px',
-        backgroundColor: disabled ? '#F5F5F5' : 'white', // 비활성화 시 배경색 변경
-        cursor: disabled ? 'not-allowed' : 'text', // 비활성화 시 커서 변경
+      disabled={disabled} // 비활성화 상태
+      multiline // 여러 줄 텍스트 필드
+      minRows={1} // 최소 줄 수 (한 줄 크기)
+      fullWidth // 전체 너비 사용
+      variant="outlined" // 아웃라인 스타일
+      InputProps={{
+        style: {
+          borderRadius: '8px',
+          border: '1px solid #BFBFBF',
+          padding: '8px 12px',
+          fontSize: '14px',
+          backgroundColor: disabled ? '#F5F5F5' : 'white', // 비활성화 시 배경색 변경
+          cursor: disabled ? 'not-allowed' : 'text', // 비활성화 시 커서 변경
+        },
       }}
-      onFocus={(e) => !disabled && (e.target.style.borderColor = '#3150EE')} // 비활성화 상태에서 포커스 방지
-      onBlur={(e) => !disabled && (e.target.style.borderColor = '#BFBFBF')}  // 비활성화 상태에서 기본 상태로
+      onFocus={(e) => !disabled && (e.target.style.borderColor = '#3150EE')} // 포커스 시 테두리 색상 변경
+      onBlur={(e) => !disabled && (e.target.style.borderColor = '#BFBFBF')} // 포커스 해제 시 기본 테두리 색상으로 변경
     />
   );
 };
