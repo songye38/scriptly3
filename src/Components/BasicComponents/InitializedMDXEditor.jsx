39a50867cc3d89261cecf0f9489f983bebbeece5
songye38 <ddev0308@gmail.com>
2024-12-25 12:58:55 +0900

    새로운 마크다운 에디터 이용

---
+++ src/Components/BasicComponents/InitializedMDXEditor.jsx
@@ -0,0 +1,31 @@
+'use client';
+import React, { forwardRef } from 'react'; // forwardRef를 사용하기 위해 import
+import {
+  headingsPlugin,
+  listsPlugin,
+  quotePlugin,
+  thematicBreakPlugin,
+  markdownShortcutPlugin,
+  MDXEditor,
+  MDXEditorMethods
+} from '@mdxeditor/editor';
+import '@mdxeditor/editor/style.css';
+
+// forwardRef를 사용하여 ref를 전달받는 컴포넌트
+const InitializedMDXEditor = forwardRef((props, ref) => {
+  return (
+    <MDXEditor
+      plugins={[
+        headingsPlugin(),
+        listsPlugin(),
+        quotePlugin(),
+        thematicBreakPlugin(),
+        markdownShortcutPlugin(),
+      ]}
+      {...props} // MDXEditor에 전달할 나머지 props들
+      ref={ref} // ref를 전달하여 MDXEditor 인스턴스를 접근할 수 있게 해줌
+    />
+  );
+});
+
+export default InitializedMDXEditor;
--- src/Components/ComplexComponents/MarkdownEditor.jsx
+++ src/Components/ComplexComponents/MarkdownEditor.jsx
@@ -1,40 +1,48 @@
-import React, { useState } from "react";
-import MDEditor from "@uiw/react-md-editor";
-import { useEffect } from "react";
+'use client';
+
+import React, { useState, useEffect, useRef } from 'react';
+import InitializedMDXEditor from '../BasicComponents/InitializedMDXEditor';
 
 const MarkdownEditor = ({ initialContent }) => {
-  const [value, setValue] = useState(initialContent);
+  const [value, setValue] = useState(initialContent || ''); // 초기값 설정
+  const ref = useRef(null); // MDXEditor에 대한 ref 설정
 
+  // initialContent가 변경되면 editor의 내용을 업데이트
+  useEffect(() => {
+    setValue(initialContent); // 상태값을 업데이트
+    if (ref.current) {
+      ref.current.setMarkdown(initialContent); // ref를 통해 setMarkdown 호출
+    }
+  }, [initialContent]); // initialContent가 변경될 때마다 실행
+
+  // editor에서 값이 변경될 때 상태를 업데이트
   const handleEditorChange = (newValue) => {
     setValue(newValue);
   };
 
   useEffect(() => {
-    setValue(initialContent);
-  }, [initialContent]); // editorContent가 변경될 때마다 호출
+    // value가 변경될 때마다 ref에서 최종값을 출력
+    if (ref.current) {
+      console.log("최종 red");
+      console.log(ref.current.getMarkdown()); // 최종 값 출력
+    }
+  }, [value]);
 
   return (
-    <div style={{ width: '75vw', height: '100vh', display: 'flex', flexDirection: 'column', backgroundColor: 'white' }}>
-      <MDEditor
-        value={value}
-        onChange={handleEditorChange}
-        style={{
-          flexGrow: 1,
-          marginBottom: '20px',
-          height: '100%',
-          backgroundColor: 'white',
-          color : 'black',
-        }}
+    <div
+      style={{
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        backgroundColor: 'white',
+      }}
+    >
+      <InitializedMDXEditor
+        ref={ref} // MDXEditor에 ref 전달
+        markdown={value} // markdown 상태 전달
+        //onChange={alert(initialContent)} // markdown이 변경될 때 호출되는 함수
       />
-      
-
-      {/* MDEditor 아이콘 크기 조정 */}
-      <style jsx>{`
-        .w-md-editor-toolbar button svg {
-          width: 24px;   /* 아이콘의 크기를 키우는 부분 */
-          height: 24px;  /* 아이콘의 높이를 키우는 부분 */
-        }
-      `}</style>
     </div>
   );
 };
